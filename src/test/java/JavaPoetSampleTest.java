b7873e54dc6eaee3142414f2c95367682804be44
maciekdeb <maciekdeb@gmail.com>
2016-01-04 16:57:52 +0100

    JavaPoet's approach test

---
+++ src/test/java/JavaPoetSampleTest.java
@@ -0,0 +1,24 @@
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
+import org.junit.Test;
+
+import javax.lang.model.element.Modifier;
+
+/**
+ * Created by maciej.debowski on 2016-01-04.
+ */
+public class JavaPoetSampleTest {
+
+    @Test
+    public void testHelloWorldClass() throws Exception {
+
+        MethodSpec main = MethodSpec.methodBuilder("main").build();
+
+        TypeSpec helloWorld = TypeSpec.classBuilder("HelloWorld")
+                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                .addMethod(main)
+                .build();
+
+        System.out.println(helloWorld);
+    }
+}
