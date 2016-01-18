b0467df7c23aab7cb6f7040c4a37a477c504c741
maciekdeb <maciekdeb@gmail.com>
2016-01-18 15:14:09 +0100

    Getters and setters for domain class generation

--- src/main/java/pl/lodz/p/bindy_generator/builder/CsvModelBuilder.java
+++ src/main/java/pl/lodz/p/bindy_generator/builder/CsvModelBuilder.java
@@ -1,15 +1,19 @@
 package pl.lodz.p.bindy_generator.builder;
 
+import com.google.common.collect.ImmutableList;
 import com.squareup.javapoet.AnnotationSpec;
 import com.squareup.javapoet.FieldSpec;
+import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.TypeSpec;
 import org.apache.camel.dataformat.bindy.annotation.CsvRecord;
 import org.apache.camel.dataformat.bindy.annotation.DataField;
+import org.apache.commons.lang3.StringUtils;
 import pl.lodz.p.bindy_generator.params.MainParams;
 import pl.lodz.p.bindy_generator.util.Config;
 import pl.lodz.p.bindy_generator.factory.AnnotationsFactory;
 
 import javax.lang.model.element.Modifier;
+import java.util.List;
 
 /**
  * Created by maciek on 08/01/16.
@@ -40,7 +44,29 @@ public class CsvModelBuilder {
                 .addAnnotation(annotation)
                 .build();
         this.csvModelBuilder.addField(field);
+
+        List<MethodSpec> methods = ImmutableList.of(createGetter(type, name), createSetter(type, name));
+        this.csvModelBuilder.addMethods(methods);
+
         return this;
+    }
+
+    private MethodSpec createGetter(Class type, String fieldName) {
+        return MethodSpec
+                .methodBuilder("get" + StringUtils.capitalize(fieldName))
+                .addModifiers(Modifier.PUBLIC)
+                .returns(type)
+                .addStatement("return $N", fieldName)
+                .build();
+    }
+
+    private MethodSpec createSetter(Class type, String fieldName) {
+        return MethodSpec
+                .methodBuilder("set" + StringUtils.capitalize(fieldName))
+                .addModifiers(Modifier.PUBLIC)
+                .addParameter(type, fieldName)
+                .addStatement("this.$N = $N", fieldName, fieldName)
+                .build();
     }
 
     public TypeSpec build() {
--- src/test/java/JavaPoetSampleTest.java
+++ src/test/java/JavaPoetSampleTest.java
@@ -1,5 +1,6 @@
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.TypeSpec;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import pl.lodz.p.bindy_generator.util.InferenceUtils;
 import pl.lodz.p.bindy_generator.util.Utils;
@@ -23,5 +24,37 @@ public class JavaPoetSampleTest {
                 .build();
 
         System.out.println(helloWorld);
+    }
+
+    @Test
+    public void testMethodGetter() {
+
+        String fieldName = "clientId";
+        Class type = String.class;
+
+        MethodSpec method = MethodSpec
+                .methodBuilder("get" + StringUtils.capitalize(fieldName))
+                .addModifiers(Modifier.PUBLIC)
+                .returns(type)
+                .addStatement("return $N", fieldName)
+                .build();
+
+        System.out.println(method);
+    }
+
+    @Test
+    public void testMethodSetter() {
+
+        String fieldName = "clientId";
+        Class type = String.class;
+
+        MethodSpec method = MethodSpec
+                .methodBuilder("set" + StringUtils.capitalize(fieldName))
+                .addModifiers(Modifier.PUBLIC)
+                .addParameter(type, fieldName)
+                .addStatement("this.$N = $N", fieldName, fieldName)
+                .build();
+
+        System.out.println(method);
     }
 }
